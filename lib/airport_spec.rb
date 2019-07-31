799772119082b8c9adb684d78d028ea8e8e3706b
Jodi <jodiallysonfoster@gmail.com>
2019-07-31 16:22:33 +0100

    Create a station class and expose zone and name

---
+++ lib/airport_spec.rb
@@ -0,0 +1,84 @@
+require "airport"
+
+describe Airport do
+
+  describe "#land(plane)" do
+    it "raises an error when landing attempted during a storm" do
+      weather = double(:weather)
+      allow(weather).to receive(:stormy?).and_return(true)
+      airport = Airport.new(weather)
+      plane = Plane.new
+      expect { airport.land(plane) }.to raise_error "Too stormy to land"
+    end
+
+    it "raises an error when there is no room to land" do
+      weather = double(:weather)
+      allow(weather).to receive(:stormy?).and_return(false)
+      tplane = double(:tplane)
+      allow(tplane).to receive(:landed?).and_return(false)
+      airport = Airport.new(weather)
+      plane = Plane.new
+      airport.capacity.times { airport.land(plane) }
+      expect { airport.land(tplane) }.to raise_error "No room to land"
+    end
+
+    it "raises an error when the plane is already landed" do
+      weather = double(:weather)
+      allow(weather).to receive(:stormy?).and_return(false)
+      plane = double(:plane)
+      allow(plane).to receive(:landed?).and_return(true)
+      airport = Airport.new(weather)
+      expect { airport.land(plane) }.to raise_error "Plane already landed"
+
+    end
+
+  end
+
+  describe "#takeoff(plane)" do
+    it "allows a plane to take off and confirm that its not at airport" do
+      weather = double(:weather)
+      allow(weather).to receive(:stormy?).and_return(false, false)
+      tplane = double(:tplane)
+      allow(tplane).to receive(:landed?).and_return(false, true)
+      airport = Airport.new(weather)
+      airport.land(tplane)
+      expect(airport.takeoff(plane)).to eq "Takeoff"
+    end
+
+    it "raises an error when takeoff attempted during a storm" do
+      weather = double(:weather)
+      allow(weather).to receive(:stormy?).and_return(false, true)
+      plane = double(:plane)
+      allow(plane).to receive(:landed?).and_return(false)
+      airport = Airport.new(weather)
+      airport.land(plane)
+      expect { airport.takeoff(plane) }.to raise_error "Too stormy to takeoff"
+    end
+
+    it "raises an error when the plane is already in the air" do
+      weather = double(:weather)
+      allow(weather).to receive(:stormy?).and_return(false)
+      plane = double(:plane)
+      allow(plane).to receive(:landed?).and_return(false)
+      airport = Airport.new(weather)
+      airport.land(plane)
+      expect { airport.takeoff(plane) }.to raise_error "Plane already in the air"
+    end
+
+    it "raises an error when there are no airplanes to takeoff" do
+      plane = double(:plane)
+      allow(plane).to receive(:landed?).and_return(false)
+      expect { subject.takeoff(plane) }.to raise_error "No planes available"
+    end
+  end
+
+  it "has a default capacity" do
+    expect(subject.capacity).to eq Airport::DEFAULT_CAPACITY
+  end
+
+  it "has a default capacity that can be overridden as appropriate" do
+    airport = Airport.new
+    airport.capacity = 25
+    expect(airport.capacity).to eq 25
+  end
+end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,5 +1,15 @@
-# require_relative '../lib/station.rb'
-#
-# describe Station do
-#
-# end
+
+require "station"
+
+describe Station do
+
+  subject {described_class.new(name = "Old Street", zone = 1)}
+
+  it "has a name" do
+    expect(subject.name).to eq("Old Street")
+  end
+
+  it "has a zone" do
+    expect(subject.zone).to eq(1)
+  end
+end
